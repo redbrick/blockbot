f54eb9821d21d7232381f023c94e7e820d99d735
hypnoant <hypnoant1@gmail.com>
2024-01-26 15:46:33 +0000

    Fixed

--- src/extensions/boosts.py
+++ src/extensions/boosts.py
@@ -8,7 +8,6 @@ class boosts(discord.Extension):
     @discord.listen()
     async def on_message(self, event: discord.events.MessageCreate):
         if event.message.type in [8,9,10,11]:
-            print(main.config['channel_ids']['lobby'])
             channel = await self.client.fetch_channel(main.config['channel_ids']['lobby'])
             await channel.send(event.message.system_content)
     
