cd96adbfaab439161fbb201782baac30a4192f50
Jed Hazaymeh <jed.hazaymeh2@mail.dcu.ie>
2024-01-29 14:43:20 +0000

    Remove deploy script files

--- tools/deploy-prod.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/local/bin/bash
-
-
-echo "${GITHUB_SHA}"
-levant deploy \
-  -var git_sha="${GITHUB_SHA}" \
-  -address "http://nomad.service.consul:4646" \
-  tools/templates/blockbot-prod.hcl
