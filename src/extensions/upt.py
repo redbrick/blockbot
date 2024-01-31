fd125506d74f9cff5b5c8c770b1b59e09d13748f
Konstantin O'D-D (xoreax) <57073484+koyakonsta@users.noreply.github.com>
2024-01-31 22:00:44 +0000

    Add basic version of /uptime command

---
+++ src/extensions/upt.py
@@ -0,0 +1,15 @@
+import time as t; starttime = t.time()
+import arc
+import hikari
+import datetime as dt
+
+upt_plugin = arc.GatewayPlugin("Blockbot Uptime")
+
+@upt_plugin.include
+@arc.slash_command("uptime", "Show formatted uptime of Blockbot")
+async def uptime(ctx):
+    await ctx.respond(f"Uptime: **{dt.timedelta(seconds=t.time()-starttime)}**")
+
+@arc.loader
+def loader(client):
+    client.add_plugin(upt_plugin)
