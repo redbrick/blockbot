cce84f538441b720779f72449af3189531bf1849
nova <110734810+novanai@users.noreply.github.com>
2024-01-31 20:30:48 +0000

    Hikari rewrite (#25)
    Co-authored-by: wizzdom <dom@wizzdom.xyz>
    Co-authored-by: XOREAX <57073484+koyakonsta@users.noreply.github.com>

---
+++ src/bot.py
@@ -0,0 +1,23 @@
+import logging
+import sys
+
+import arc
+import hikari
+
+from src.config import DEBUG, TOKEN
+
+if TOKEN is None:
+    print("TOKEN environment variable not set. Exiting.")
+    sys.exit(1)
+
+bot = hikari.GatewayBot(
+    token=TOKEN,
+    banner=None,
+    intents=hikari.Intents.ALL_UNPRIVILEGED | hikari.Intents.MESSAGE_CONTENT,
+    logs="DEBUG" if DEBUG else "INFO",
+)
+
+logging.info(f"Debug mode is {DEBUG}; You can safely ignore this.")
+
+arc_client = arc.GatewayClient(bot, is_dm_enabled=False)
+arc_client.load_extensions_from("./src/extensions/")
--- src/config.py
+++ src/config.py
@@ -1,7 +1,10 @@
 import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
-TOKEN = os.environ.get("TOKEN") # required
+TOKEN = os.environ.get("TOKEN")  # required
 DEBUG = os.environ.get("DEBUG", False)
+
+CHANNEL_IDS = {"lobby": 627542044390457350}
