7b60f6c373fbffde8b1ef37eaa4df2ba20513974
Konstantin O'D-D (xoreax) <57073484+koyakonsta@users.noreply.github.com>
2024-02-01 00:58:31 +0100

    ROLE_IDS -> list of tuples

--- src/config.py
+++ src/config.py
@@ -7,10 +7,10 @@ load_dotenv()
 TOKEN = os.environ.get("TOKEN")  # required
 DEBUG = os.environ.get("DEBUG", False)
 
-ROLE_IDS = {
-  "Webgroup": 1166751688598761583,
-  "Gamez": 1089204642241581139,
-  "Croomer": 1172696659097047050
-}
+ROLE_IDS = [
+  ("Webgroup", 1166751688598761583),
+  ("Gamez", 1089204642241581139),
+  ("Croomer", 1172696659097047050)
+  ]
 
 CHANNEL_IDS = {"lobby": 627542044390457350}
