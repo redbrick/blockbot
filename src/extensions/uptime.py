bec3143a7d1a73b67761b9ddf1e683c39e8a3d12
Konstantin O'D-D (xoreax) <57073484+koyakonsta@users.noreply.github.com>
2024-02-01 11:21:15 +0100

    Nobody likes microseconds {{{(>_<)}}}

--- src/extensions/uptime.py
+++ src/extensions/uptime.py
@@ -11,7 +11,7 @@ plugin = arc.GatewayPlugin("Blockbot Uptime")
 @plugin.include
 @arc.slash_command("uptime", "Show formatted uptime of Blockbot")
 async def uptime(ctx):
-    await ctx.respond(f"Uptime: **{dt.timedelta(seconds=t.time()-start_time)!s}**")
+    await ctx.respond(f"Uptime: **{dt.timedelta(seconds=(t.time()-start_time) // 1)!s}**")
 
 @arc.loader
 def loader(client):
