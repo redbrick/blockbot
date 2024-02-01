33f3888304cbc3651584bbd797067669d6c02063
Konstantin O'D-D (xoreax) <57073484+koyakonsta@users.noreply.github.com>
2024-02-01 10:06:05 +0000

    Merge /uptime fork into branch  (#34)
    Co-authored-by: James Hackett <jamesthackett1@gmail.com>

---
+++ src/extensions/uptime.py
@@ -0,0 +1,18 @@
+import time as t
+
+start_time = t.time()
+
+import arc
+import hikari
+import datetime as dt
+
+plugin = arc.GatewayPlugin("Blockbot Uptime")
+
+@plugin.include
+@arc.slash_command("uptime", "Show formatted uptime of Blockbot")
+async def uptime(ctx):
+    await ctx.respond(f"Uptime: **{dt.timedelta(seconds=t.time()-start_time)!s}**")
+
+@arc.loader
+def loader(client):
+    client.add_plugin(plugin)
