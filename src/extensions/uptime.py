67268443293f92bc23d035a918940d2b8e4c7983
Konstantin O'D-D (xoreax) <57073484+koyakonsta@users.noreply.github.com>
2024-02-02 00:54:26 +0100

    /uptime timestamp string formatting update
    Co-authored-by: Jed Hazaymeh <jed.hazaymeh2@mail.dcu.ie>

--- src/extensions/uptime.py
+++ src/extensions/uptime.py
@@ -10,9 +10,15 @@ plugin = arc.GatewayPlugin("Blockbot Uptime")
 @arc.slash_command("uptime", "Show formatted uptime of Blockbot")
 async def uptime(ctx):
     up_time = datetime.now() - start_time
+    d = up_time.days
     h, ms = divmod(up_time.seconds, 3600)
     m, s = divmod(ms, 60)
-    await ctx.respond(f"Uptime: **{up_time.days} days, {h} hours, {m} minutes, and {s} seconds**.")
+
+    format = lambda val, str: f"{val} {str}{'s' if val != 1 else ''}"
+    message_parts = [(d, "day"), (h, "hour"), (m, "minute"), (s, "second")]
+    formatted_parts = [format(val, str) for val, str in message_parts if val]
+    
+    await ctx.respond(f"Uptime: **{', '.join(formatted_parts)}**")
 
 @arc.loader
 def loader(client):
