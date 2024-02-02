32293161e964e5b628ed0823330ad622755fdd84
Konstantin O'D-D (xoreax) <57073484+koyakonsta@users.noreply.github.com>
2024-02-02 00:03:29 +0000

    Add /uptime command (#35)
    Co-authored-by: James Hackett <jamesthackett1@gmail.com>
    Co-authored-by: Regaus <42419910+Regaus@users.noreply.github.com>
    Co-authored-by: nova <110734810+novanai@users.noreply.github.com>
    Co-authored-by: Jed Hazaymeh <jed.hazaymeh2@mail.dcu.ie>

---
+++ src/extensions/uptime.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+import arc
+
+start_time = datetime.now()
+
+plugin = arc.GatewayPlugin("Blockbot Uptime")
+
+@plugin.include
+@arc.slash_command("uptime", "Show formatted uptime of Blockbot")
+async def uptime(ctx):
+    up_time = datetime.now() - start_time
+    d = up_time.days
+    h, ms = divmod(up_time.seconds, 3600)
+    m, s = divmod(ms, 60)
+
+    format = lambda val, str: f"{val} {str}{'s' if val != 1 else ''}"
+    message_parts = [(d, "day"), (h, "hour"), (m, "minute"), (s, "second")]
+    formatted_parts = [format(val, str) for val, str in message_parts if val]
+    
+    await ctx.respond(f"Uptime: **{', '.join(formatted_parts)}**")
+
+@arc.loader
+def loader(client):
+    client.add_plugin(plugin)
