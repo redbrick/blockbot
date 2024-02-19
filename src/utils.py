3e654f7bb784a62dafab66cff13662c761f2de93
nova <novanai.dev@gmail.com>
2024-02-19 09:32:03 +0000

    Fix typing

--- src/utils.py
+++ src/utils.py
@@ -2,9 +2,11 @@ import hikari
 from arc import GatewayClient
 
 
-async def get_guild(client: GatewayClient, event: hikari.GuildMessageCreateEvent):
+async def get_guild(
+    client: GatewayClient, event: hikari.GuildMessageCreateEvent
+) -> hikari.GatewayGuild | hikari.RESTGuild:
     return event.get_guild() or await client.rest.fetch_guild(event.guild_id)
 
 
-def role_mention(role_id: hikari.Snowflake | int | str):
+def role_mention(role_id: hikari.Snowflake | int | str) -> str:
     return f"<@&{role_id}>"
