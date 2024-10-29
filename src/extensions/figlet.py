c192e720d0ed5d08f201e7d13ba5ab2a1c1a2479
wizzdom <dom@wizzdom.xyz>
2024-10-29 00:38:28 +0000

    figlet: list for fonts, length check after codeblock

--- src/extensions/figlet.py
+++ src/extensions/figlet.py
@@ -1,28 +1,27 @@
 import arc
 import hikari
 
-
 from pyfiglet import Figlet
 
 plugin = arc.GatewayPlugin(name="figlet")
 
-style_choices = [
-    hikari.CommandChoice(name="Default", value="standard"),
-    hikari.CommandChoice(name="Slant", value="slant"),
-    hikari.CommandChoice(name="weird", value="stanpatello"),
-    hikari.CommandChoice(name="USA", value="usaflag"),
-    hikari.CommandChoice(name="CyberMedium", value="cybermedium"),
-    hikari.CommandChoice(name="wideterm", value="wideterm"),
-    hikari.CommandChoice(name="Lean", value="lean"),
-    hikari.CommandChoice(name="3-D", value="3-d"),
-    hikari.CommandChoice(name="larry3d", value="larry3d"),
-    hikari.CommandChoice(name="Letters", value="letters"),
-    hikari.CommandChoice(name="Poison", value="poison"),
-    hikari.CommandChoice(name="doh", value="doh"),
-    hikari.CommandChoice(name="epic", value="epic"),
-    hikari.CommandChoice(name="fraktur", value="fraktur"),
-    hikari.CommandChoice(name="graffiti", value="graffiti"),
-    hikari.CommandChoice(name="katakana", value="katakana"),
+fonts = [
+    "standard",
+    "slant",
+    "stanpatello",
+    "usaflag",
+    "cybermedium",
+    "wideterm",
+    "lean",
+    "3-d",
+    "larry3d",
+    "letters",
+    "poison",
+    "doh",
+    "epic",
+    "fraktur",
+    "graffiti",
+    "katakana",
 ]
 
 
@@ -31,30 +30,38 @@ style_choices = [
 async def figlet_command(
     ctx: arc.GatewayContext,
     text: arc.Option[str, arc.StrParams("Your words to ASCIIify.")],
-    style: arc.Option[
+    font: arc.Option[
         str,
-        arc.StrParams("The style of your ASCII text", choices=style_choices),
+        arc.StrParams(
+            "The style of your ASCII text",
+            choices=[
+                hikari.CommandChoice(name=font.capitalize(), value=font)
+                for font in fonts
+            ],
+        ),
     ] = "standard",
 ) -> None:
     """ASCIIify words"""
 
-    figlet = Figlet(font=style)
+    # create Figlet object with the selected font
+    figlet = Figlet(font=font)
 
-    try:
-        # generate ASCII art
-        ascii_art = figlet.renderText(text)
-    except Exception as e:
-        await ctx.respond(f"Failed to generate ASCII art: {e}")
-        return
+    # generate ASCII art
+    ascii_art = figlet.renderText(text)
+
+    # wrap ASCII art in a codeblock
+    message = f"```{ascii_art}```"
 
     # do not exceed Discord's 2000 character limit
-    if len(ascii_art) > 2000:
+    if len(message) > 2000:
         await ctx.respond(
-            "The generated ASCII art is too long to send. Please try a shorter text."
+            "The generated ASCII art is too long to send. Please try a shorter text.",
+            flags=hikari.MessageFlag.EPHEMERAL,
         )
+
     else:
         # send ASCII art in a codeblock
-        await ctx.respond(f"```\n{ascii_art}\n```")
+        await ctx.respond(message)
 
 
 @arc.loader
