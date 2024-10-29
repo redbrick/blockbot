14c8ed67de4855e5717b402ac19e43051efb4e18
wizzdom <wizzdom@redbrick.dcu.ie>
2024-10-29 17:25:39 +0000

    add /gerry command (#49)

---
+++ src/extensions/gerry.py
@@ -0,0 +1,34 @@
+import arc
+import hikari
+
+gerry = arc.GatewayPlugin(name="gerry")
+
+image = "https://i.redd.it/cu94ebjex3481.jpg"
+
+
+@gerry.include
+@arc.slash_command("gerry", "So tell me Frank!")
+async def gerry_command(
+    ctx: arc.GatewayContext,
+    user: arc.Option[
+        hikari.User, arc.UserParams("The user to send a gerry to.")
+    ] = None,
+) -> None:
+    """Send a gerry!"""
+    if user is not None:
+        description = f"So tell me {user.mention}!"
+    else:
+        description = "So tell me Frank!"
+    embed = hikari.Embed(
+        title="Gerry",
+        description=description,
+        colour=0x5865F2,
+    )
+    embed = embed.set_image(image)
+
+    await ctx.respond(embed)
+
+
+@arc.loader
+def loader(client: arc.GatewayClient) -> None:
+    client.add_plugin(gerry)
