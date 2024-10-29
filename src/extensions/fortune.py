a329e04ff0caa68cdc294b2d0d0a04420c7a2e25
wizzdom <dom@wizzdom.xyz>
2024-10-29 15:45:09 +0000

    remove error handler, add more checks

--- src/extensions/fortune.py
+++ src/extensions/fortune.py
@@ -11,12 +11,18 @@ async def fortune_command(
     ctx: arc.GatewayContext,
     user: arc.Option[hikari.User, arc.UserParams("A user")] = None,
 ) -> None:
-    """Send a user a random Fortune!"""
+    """Send a random Fortune!"""
 
     # generate fortune
     fortune_message = fortune()
-    assert len(fortune_message) > 0
+    if not fortune_message:
+        await ctx.respond(
+            "❌ Failed to generate fortune. Please try again.",
+            flags=hikari.MessageFlag.EPHEMERAL,
+        )
+        return
 
+    # mention user if present
     if user is not None:
         message = f"Dear {user.mention},\n```{fortune_message}```"
     else:
@@ -25,32 +31,13 @@ async def fortune_command(
         # do not exceed Discord's 2000 character limit
     if len(message) > 2000:
         await ctx.respond(
-            "The generated fortune is too long to send. Please try again.",
+            "❌ FThe generated fortune is too long to send. Please try again.",
             flags=hikari.MessageFlag.EPHEMERAL,
         )
+        return
 
     # send fortune in a codeblock
     await ctx.respond(message)
-
-
-@fortune_cmd.set_error_handler
-async def fortune_error_handler(ctx: arc.GatewayContext, exc: Exception) -> None:
-    user = ctx.get_option("user", arc.OptionType.USER)
-    assert user is not None
-
-    if isinstance(exc, AssertionError):
-        await ctx.respond(
-            "❌ Failed to generate the fortune. Please try again.",
-            flags=hikari.MessageFlag.EPHEMERAL,
-        )
-        return
-    if isinstance(exc, hikari.NotFoundError):
-        await ctx.respond(
-            "❌ Blockbot can't find that user.", flags=hikari.MessageFlag.EPHEMERAL
-        )
-        return
-
-    raise exc
 
 
 @arc.loader
