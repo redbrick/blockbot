f473897da3501f2f69d443aeeecd023c2bce5a1b
Ayden Jahola <jaholaayden@gmail.com>
2024-11-12 12:17:19 +0000

    format the code

--- src/extensions/help.py
+++ src/extensions/help.py
@@ -4,14 +4,18 @@ import itertools
 
 plugin = arc.GatewayPlugin(name="Help Command Plugin")
 
+
 def gather_commands(client: arc.GatewayClient):
     command_list = []
     for command in itertools.chain(client.walk_commands(hikari.CommandType.SLASH)):
-        command_list.append({
-            "name": command.name,
-            "description": command.description or "No description provided."
-        })
+        command_list.append(
+            {
+                "name": command.name,
+                "description": command.description or "No description provided.",
+            }
+        )
     return command_list
+
 
 @plugin.include
 @arc.slash_command("help", "Displays a list of all commands.")
@@ -22,9 +26,12 @@ async def help_command(ctx: arc.GatewayContext) -> None:
     embed = hikari.Embed(title="Bot Commands", color=0x00FF00)
 
     for command in commands:
-        embed.add_field(name=f"/{command['name']}", value=command['description'], inline=False)
+        embed.add_field(
+            name=f"/{command['name']}", value=command["description"], inline=False
+        )
 
     await ctx.respond(embed=embed)
+
 
 @arc.loader
 def load(client: arc.GatewayClient) -> None:
