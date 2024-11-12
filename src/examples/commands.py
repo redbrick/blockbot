65d1d955f69fcdb49f58d9529f0e61956d24971f
nova <110734810+novanai@users.noreply.github.com>
2024-11-12 11:24:27 +0000

    Update README & refactor examples (#44)
    Co-authored-by: Ayden <jaholaayden@gmail.com>

---
+++ src/examples/commands.py
@@ -0,0 +1,35 @@
+import arc
+
+plugin = arc.GatewayPlugin(name="Example Commands")
+
+
+@plugin.include
+@arc.slash_command("hello", "Say hello!")
+async def hello(ctx: arc.GatewayContext) -> None:
+    """An individual command, invoked by `/hello`."""
+    await ctx.respond("Hello from hikari and hikari-arc!")
+
+
+group = plugin.include_slash_group("base_group", "A base command group, with sub groups and sub commands.")
+
+
+@group.include
+@arc.slash_subcommand("sub_command", "A sub command")
+async def sub_command(ctx: arc.GatewayContext) -> None:
+    """A subcommand, invoked by `/base_command sub_command`."""
+    await ctx.respond("Hello, world! This is a sub command")
+
+
+sub_group = group.include_subgroup("sub_group", "A subgroup to add commands to.")
+
+
+@sub_group.include
+@arc.slash_subcommand("sub_command", "A subgroup subcommand.")
+async def sub_group_sub_command(ctx: arc.GatewayContext) -> None:
+    """A subcommand belonging to a subgroup, invoked by `/base_group sub_group sub_command`."""
+    await ctx.respond("This is a subgroup subcommand.")
+
+
+@arc.loader
+def loader(client: arc.GatewayClient) -> None:
+    client.add_plugin(plugin)
---
+++ src/examples/options.py
@@ -0,0 +1,32 @@
+import arc
+import hikari
+
+plugin = arc.GatewayPlugin("Example Options")
+
+
+# Options Guide: https://arc.hypergonial.com/guides/options/
+@plugin.include
+@arc.slash_command("options", "A command with options")
+async def options(
+    ctx: arc.GatewayContext,
+    str_option: arc.Option[str, arc.StrParams("A string option.", name="string")],
+    int_option: arc.Option[int, arc.IntParams("An integer option.", name="integer", min=5, max=150)],
+    attachment_option: arc.Option[hikari.Attachment, arc.AttachmentParams("An attachment option.", name="attachment")],
+    channel_option: arc.Option[
+        hikari.TextableChannel | None, arc.ChannelParams("A textable channel option.", name="channel")
+    ] = None,
+) -> None:
+    """A command with lots of options."""
+    embed = hikari.Embed(title="There are a lot of options here", description="Maybe too many...", colour=0x5865F2)
+    embed.set_image(attachment_option)
+
+    embed.add_field("String option", str_option, inline=True)
+    embed.add_field("Integer option", str(int_option), inline=True)
+    embed.add_field("Channel option", f"<#{channel_option.id}>" if channel_option else "Not supplied", inline=True)
+
+    await ctx.respond(embed=embed)
+
+
+@arc.loader
+def loader(client: arc.GatewayClient) -> None:
+    client.add_plugin(plugin)
