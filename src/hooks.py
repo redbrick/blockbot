72dac79fbbb06bb83cfb14233c0f985e58973f2d
nova <novanai.dev@gmail.com>
2025-03-26 09:52:46 +0000

    update permission hooks disabling

--- src/hooks.py
+++ src/hooks.py
@@ -12,25 +12,31 @@ type WrappedHookResult = typing.Callable[
 ]
 
 
+def can_be_disabled(func: WrappedHookResult) -> WrappedHookResult:
+    async def wrapper(ctx: BlockbotContext) -> arc.HookResult:
+        if not PERMS_ENABLED:
+            logging.warning(
+                f"permission hook disabled; bypassing restrictions for '{ctx.command.name}' command"
+            )
+            return arc.HookResult(abort=False)
+
+        return await func(ctx)
+
+    return wrapper
+
+
 async def _restrict_to_roles(
     ctx: BlockbotContext,
     role_ids: typing.Sequence[int],
 ) -> arc.HookResult:
     assert ctx.member
 
-    if PERMS_ENABLED and not any(
-        role_id in ctx.member.role_ids for role_id in role_ids
-    ):
+    if not all(role_id in ctx.member.role_ids for role_id in role_ids):
         await ctx.respond(
             "❌ This command is restricted. Only allowed roles are permitted to use this command.",
             flags=hikari.MessageFlag.EPHEMERAL,
         )
         return arc.HookResult(abort=True)
-
-    if not PERMS_ENABLED:
-        logging.warning(
-            f"permission hooks disabled; bypassing role restrictions for '{ctx.command.name}' command"
-        )
 
     return arc.HookResult()  # by default, abort is set to False
 
@@ -40,6 +46,7 @@ def restrict_to_roles(
 ) -> WrappedHookResult:
     """Any command which uses this hook requires that the command be disabled in DMs as a guild role is required for this hook to function."""
 
+    @can_be_disabled
     async def func(ctx: BlockbotContext) -> arc.HookResult:
         return await _restrict_to_roles(ctx, role_ids)
 
@@ -50,17 +57,12 @@ async def _restrict_to_channels(
     ctx: BlockbotContext,
     channel_ids: typing.Sequence[int],
 ) -> arc.HookResult:
-    if PERMS_ENABLED and ctx.channel_id not in channel_ids:
+    if ctx.channel_id not in channel_ids:
         await ctx.respond(
             "❌ This command cannot be used in this channel.",
             flags=hikari.MessageFlag.EPHEMERAL,
         )
         return arc.HookResult(abort=True)
-
-    if not PERMS_ENABLED:
-        logging.warning(
-            f"permission hooks disabled; bypassing channel restrictions for '{ctx.command.name}' command"
-        )
 
     return arc.HookResult()
 
@@ -68,6 +70,7 @@ async def _restrict_to_channels(
 def restrict_to_channels(
     channel_ids: typing.Sequence[int],
 ) -> WrappedHookResult:
+    @can_be_disabled
     async def func(ctx: BlockbotContext) -> arc.HookResult:
         return await _restrict_to_channels(ctx, channel_ids)
 
