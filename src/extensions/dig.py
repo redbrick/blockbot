64527955830740a944be41a31b2758ca207571f7
wizzdom <dom@wizzdom.xyz>
2025-03-26 05:22:58 +0000

    add dig command

---
+++ src/extensions/dig.py
@@ -0,0 +1,42 @@
+import arc
+import hikari
+from dns import resolver
+
+dig = arc.GatewayPlugin(name="dig")
+
+
+@dig.include
+@arc.slash_command("dig", "Query DNS records for a domain")
+async def dig_command(
+    ctx: arc.GatewayContext,
+    domain: arc.Option[str, arc.StrParams("The domain to query")],
+    record_type: arc.Option[
+        str,
+        arc.StrParams(
+            "Type of record to query",
+            choices=["A", "AAAA", "CNAME", "MX", "NS", "PTR", "SOA", "SRV", "TXT"],
+        ),
+    ] = "A",
+) -> None:
+    """Query DNS records for a domain"""
+    record_type = record_type.upper()
+
+    try:
+        response = resolver.resolve(domain, record_type)
+    except resolver.NoAnswer as e:
+        await ctx.respond(f"❌ {e}", flags=hikari.MessageFlag.EPHEMERAL)
+        return
+
+    result = "\n".join(str(rdata) for rdata in response)
+    message = f"""
+```
+{record_type} records for {domain}:
+{result}
+```
+"""
+    await ctx.respond(message)
+
+
+@arc.loader
+def loader(client: arc.GatewayClient) -> None:
+    client.add_plugin(dig)
