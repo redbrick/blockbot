94d38361e6ee5359b631f315e8485809a9a0632d
wizzdom <wizzdom@redbrick.dcu.ie>
2025-03-26 11:16:28 +0000

    boosts: update formatting, add user mention (#87)

--- src/extensions/boosts.py
+++ src/extensions/boosts.py
@@ -27,16 +27,16 @@ def build_boost_message(
 ) -> str:
     assert message_type in BOOST_MESSAGE_TYPES
 
-    base_message = f"{booster_user.display_name} just boosted the server"
+    base_message = f"<@{booster_user.id}> just boosted the server"
     multiple_boosts_message = (
         f" **{number_of_boosts}** times" if number_of_boosts else ""
     )
 
-    message = base_message + multiple_boosts_message + "!"
+    message = base_message + multiple_boosts_message + "! 🎉"
 
     if message_type in BOOST_TIERS:
         count = BOOST_TIERS.index(message_type) + 1
-        message += f"\n{guild.name} has reached **Level {count}!**"
+        message += f"\n**{guild.name}** has reached **Level {count}!**"
 
     return message
 
