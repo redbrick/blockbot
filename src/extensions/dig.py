0b78c148c3dd3b2514b60c451b98f7c180352213
wizzdom <wizzdom@redbrick.dcu.ie>
2025-12-15 18:36:44 +0000

    add dig command (#86)
    Co-authored-by: nova <novanai.dev@gmail.com>

---
+++ src/extensions/dig.py
@@ -0,0 +1,41 @@
+import arc
+import hikari
+from dns import asyncresolver, resolver
+
+from src.models import Blockbot, BlockbotContext, BlockbotPlugin
+
+plugin = BlockbotPlugin(name="dig")
+
+
+@plugin.include
+@arc.slash_command("dig", "Query DNS records for a domain")
+async def dig_command(
+    ctx: BlockbotContext,
+    domain: arc.Option[str, arc.StrParams("The domain to query")],
+    record_type: arc.Option[
+        str,
+        arc.StrParams(
+            "Type of record to query. Default: A",
+            choices=["A", "AAAA", "CNAME", "MX", "NS", "PTR", "SOA", "SRV", "TXT"],
+        ),
+    ] = "A",
+) -> None:
+    """Query DNS records for a domain"""
+
+    try:
+        response = await asyncresolver.resolve(domain, record_type)
+    except (resolver.NoAnswer, resolver.NXDOMAIN) as e:
+        await ctx.respond(f"❌ {e}", flags=hikari.MessageFlag.EPHEMERAL)
+        return
+
+    result = "\n".join(str(rdata) for rdata in response)
+    embed = hikari.Embed(
+        title=f"{record_type} records for `{domain}`:",
+        description=f"""```{result}```""",
+    )
+    await ctx.respond(embed=embed)
+
+
+@arc.loader
+def loader(client: Blockbot) -> None:
+    client.add_plugin(plugin)
