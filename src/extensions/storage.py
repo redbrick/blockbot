91185ea3eaa6ea98727f0e9c48fffedefaca645e
Aris <64918822+Arisamiga@users.noreply.github.com>
2026-02-09 00:19:02 +0000

    Improvements and initial file uploading

--- src/extensions/storage.py
+++ src/extensions/storage.py
@@ -1,5 +1,8 @@
+import io
+
 import arc
 import hikari
+import aiohttp
 from minio import Minio
 from minio.error import S3Error
 
@@ -7,57 +10,72 @@ from src.models import Blockbot, BlockbotContext, BlockbotPlugin
 
 minio = BlockbotPlugin(name="minio")
 
+minio_client = Minio(
+    "host.docker.internal:9000",
+    access_key="minioadmin",
+    secret_key="minioadmin",
+    secure=False
+)
+
 @minio.include
 @arc.slash_command("upload", "Upload a file to the storage server.")
 async def upload_command(
         ctx: BlockbotContext,
-        # file: arc.Option[hikari.Attachment, arc.AttachmentParams("The file to upload.")],
+        file: arc.Option[hikari.Attachment, arc.AttachmentParams("The file to upload.")],
+        aiohttp_client: aiohttp.ClientSession = arc.inject(),
 ) -> None:
-    minio_client = Minio(
-        "host.docker.internal:9000",
-        access_key="minioadmin",
-        secret_key="minioadmin123",
-        secure=False
+
+    if not file:
+        await ctx.respond(
+            "❌ Please attach a file to upload.",
+            flags=hikari.MessageFlag.EPHEMERAL,
+        )
+        return
+
+    # The file to upload, change this path if needed
+    source_file = file.url
+
+    # await ctx.respond(minio_client.list_buckets() or "Nope")
+
+    # The destination bucket and filename on the MinIO server
+    bucket_name = "python-test-bucket"
+
+    # Make the bucket if it doesn't exist.
+    found = minio_client.bucket_exists(bucket_name)
+    if not found:
+        minio_client.make_bucket(bucket_name)
+        await ctx.respond(
+            f"✅ Created bucket `{bucket_name}` on the storage server.",
+            flags=hikari.MessageFlag.EPHEMERAL,
+        )
+    else:
+        await ctx.respond(
+            f"✅ Bucket `{bucket_name}` Exists on the storage server.",
+            flags=hikari.MessageFlag.EPHEMERAL,
+        )
+
+
+    # Temporarily download the file to a local path before uploading to MinIO
+    local_path = f"/tmp/{file.filename}"
+    async with aiohttp_client.get(source_file) as response:
+        if response.status != 200:
+            await ctx.respond(
+                f"❌ Failed to download the file. Status code: `{response.status}`",
+                flags=hikari.MessageFlag.EPHEMERAL,
+            )
+            return
+
+        data = await response.read()
+        minio_client.put_object(
+            bucket_name,
+            file.filename,
+            io.BytesIO(data),
+            len(data)
+        )
+    await ctx.respond(
+        f"✅ File `{file.filename}` uploaded successfully as `{file.filename}`.",
+        flags=hikari.MessageFlag.EPHEMERAL,
     )
-
-    # if not file:
-    #     await ctx.respond(
-    #         "❌ Please attach a file to upload.",
-    #         flags=hikari.MessageFlag.EPHEMERAL,
-    #     )
-    #     return
-    #
-    # # The file to upload, change this path if needed
-    # source_file = file.url
-
-    await ctx.respond(minio_client.list_buckets() or "Nope")
-    #
-    # # The destination bucket and filename on the MinIO server
-    # bucket_name = "python-test-bucket"
-    # destination_file = "my-test-file.webp"
-    #
-    # # Make the bucket if it doesn't exist.
-    # found = minio_client.bucket_exists(bucket_name)
-    # if not found:
-    #     minio_client.make_bucket(bucket_name)
-    #     await ctx.respond(
-    #         f"✅ Created bucket `{bucket_name}` on the storage server.",
-    #         flags=hikari.MessageFlag.EPHEMERAL,
-    #     )
-    # else:
-    #     await ctx.respond(
-    #         f"✅ Bucket `{bucket_name}` Exists on the storage server.",
-    #         flags=hikari.MessageFlag.EPHEMERAL,
-    #     )
-
-    # # Upload the file, renaming it in the process
-    # minio_client.fput_object(
-    #     bucket_name, destination_file, source_file,
-    # )
-    # await ctx.respond(
-    #     f"✅ File `{file.filename}` uploaded successfully as `{destination_file}`.",
-    #     flags=hikari.MessageFlag.EPHEMERAL,
-    # )
 
 
 
