ef82f5c79e3552cb61f5d2c7919e314982b641ba
a-Digi-a <ShayneM2006@proton.me>
2026-06-15 17:33:46 +0100

    use hook for command permission

--- src/extensions/ticket.py
+++ src/extensions/ticket.py
@@ -7,12 +7,14 @@ from hikari.interactions.interaction_components import (
 )
 
 from src.config import CATEGORY_IDS, ROLE_IDS
+from src.hooks import restrict_to_roles
 from src.models import Blockbot, BlockbotContext, BlockbotPlugin
 
 plugin = BlockbotPlugin("Ticketing System")
 
 
 @plugin.include
+@arc.with_hook(restrict_to_roles(role_ids=[ROLE_IDS["committee"]]))
 @arc.slash_command("create_ticket_message", "Create Ticket Message")
 async def options(
     ctx: BlockbotContext,
@@ -21,13 +23,6 @@ async def options(
         arc.ChannelParams("A textable channel option.", name="channel"),
     ],
 ) -> None:
-    assert ctx.member is not None
-    if ROLE_IDS["admins"] not in ctx.member.role_ids:
-        await ctx.respond(
-            "You don't have permission to use this command!",
-            flags=hikari.MessageFlag.EPHEMERAL,
-        )
-        return
     embed = hikari.Embed(title="Tickets", description="Create a ticket here!")
     row = se.MessageActionRowBuilder()
     row.add_interactive_button(
